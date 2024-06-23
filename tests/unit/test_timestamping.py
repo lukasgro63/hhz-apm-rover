7a2bb0db34a4f766823999290b5e7f5568cd3c13
lukasgro63 <76912397+lukasgro63@users.noreply.github.com>
2024-06-23 12:57:36 +0200

    time stamping done

--- tests/unit/test_timestamping.py
+++ tests/unit/test_timestamping.py
@@ -1,11 +1,17 @@
 import pytest
 from datetime import datetime
-from src.timestamping import add_timestamp
+from src.timestamping import Timestamping
 
 class TestTimestamping:
-    def test_add_timestamp(self):
+    @pytest.fixture
+    def timestamping(self):
+        return Timestamping()
+
+    def test_add_timestamp(self, timestamping):
         """Testet, ob das Hinzufügen eines Zeitstempels das Format und die Aktualität korrekt wiedergibt."""
         test_image_name = "test_image.jpg"
-        timestamped_name = add_timestamp(test_image_name)
+        timestamped_name = timestamping.add_timestamp(test_image_name)
+        
+        # Überprüfen, ob der Zeitstempel korrekt formatiert ist
         assert datetime.now().strftime("%Y-%m-%d") in timestamped_name
-        assert test_image_name in timestamped_name
+        assert test_image_name.split('.')[0] in timestamped_name
